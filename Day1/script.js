60fb00a9a84f39c272ad87cff4a726c8c67e7f12
sarva1210 <sarva.vish77@gmail.com>
2025-10-27 20:40:50 +0530

    day1

---
+++ Day1/script.js
@@ -0,0 +1,23 @@
+{
+    var b=20;
+}
+console.log(b); // can work outside the bracket also // functionnn
+
+{
+    let a= 12;
+    console.log(a); // works only under bracket
+}
+
+const discount=100; //const is the value we cannot change
+
+console.log("hey") // Used to print or show any message in the browser console //helps you check output or debug code.
+console.info("heyyy") // thode alag tarike se show krte hai unke aage i sign aajate hai kuch kuch browser mai
+console.warn("hacked"); // Used to show warning messages in the console. It usually appears in yellow color.
+console.error("hacked");// Used to show error messages in the console. appears in red color — helps find mistakes or serious problems.
+console.table({name:"sarva"}); // Used to display data in table form (rows & columns) in the console.
+
+let c=prompt("Name?"); //Used to take input from the user through a pop-up box.
+console.log(c); 
+
+alert("be happy , i like it when u smile") // Used to show a pop-up message to the user.It only displays info, the user can only click OK (no input).
+
