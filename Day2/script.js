01a4e5593cf3c8e4996fb97cf01e64468e7f0e71
sarva1210 <sarva.vish77@gmail.com>
2025-10-27 21:40:01 +0530

    Day2

---
+++ Day2/script.js
@@ -0,0 +1,10 @@
+// data types
+// sarva - string
+// 12 - number
+// true, false - boolean
+// 1.2 - float   12.3456 - double
+// null - jab aapke pass koi value na ho dene ke liye tab use hota hai
+// arrays - used to hold more than 1 value
+// object - used to hold more than 1 property
+// undefined - jab value nhi dete tab yeh aata hai
+// NaN - it appears when you try to do a math operation on non-numeric data.
