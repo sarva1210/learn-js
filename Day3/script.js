8b007e9c891c7c67c8e7fb7bdd83c8e0a8a2ab82
sarva1210 <sarva.vish77@gmail.com>
2025-10-29 20:31:35 +0530

    js assignment

---
+++ Day3/script.js
@@ -0,0 +1,21 @@
+// js assignment
+console.log("Connected!!")
+
+let a = prompt("name?");
+alert (`hey ${a}`);
+
+let age = prompt("enter age: ");
+console.log(age + 5);
+
+var b = "harsh";
+let c = "sheryians";
+const d = "school";
+b = "Updated"; 
+c = "Updated"; 
+d = "Updated";
+
+{
+    let e = 12;
+}
+console.log(d);
+
