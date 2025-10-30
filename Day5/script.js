f1177fa2e6c6d9fc01d5b30a15417d7877b1c0e4
sarva1210 <sarva.vish77@gmail.com>
2025-10-30 21:13:02 +0530

    assignment js

---
+++ Day5/script.js
@@ -0,0 +1,57 @@
+// assignment js
+a = 10;
+b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+x = 5;
+x = x+3;
+console.log(x);
+x +=3;
+console.log(x);
+x -=3;
+console.log(x);
+x *=3;
+console.log(x);
+x /=3;
+console.log(x);
+
+let count = 5;
+console.log(count);
+count++;
+console.log(count);
+++count;
+console.log(count);
+count--;
+console.log(count);
+--count;
+console.log(count);
+
+let y = 10;
+if(y>5 && y<20 && y===10){
+    console.log("all values are true");
+}else{
+    console.log("false");
+}
+
+console.log(a);
+var c = 10;
+
+console.log(b);
+let d = 10;
+
+test();
+function test() {
+    console.log("hello");
+}
+
+// hello();
+// var hello = function(){
+//     console.log("hi");
+// }
+
+// variable gets hoisted like var let and const get hoisted but is initialised
+//fuctions are not hoisted fully
